c1aae79c4c6bf33810be0e732e35a11525e80148
monkeywithbanana <>
2019-03-31 18:43:55 -0700

    Adding solution and week 1 project

---
+++ 01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace week1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
