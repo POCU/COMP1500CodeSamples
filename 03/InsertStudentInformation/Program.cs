f454d8786e807a902dbd0a9d844d771ce511934d
monkeywithbanana <>
2019-04-03 20:57:06 -0700

    more code samples

--- 03/InsertStudentInformation/Program.cs
+++ 03/InsertStudentInformation/Program.cs
@@ -6,7 +6,21 @@ namespace InsertStudentInformation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Let's fill out student information:");
+
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Student Number: ");
+            string studentNumberString = Console.ReadLine();
+            int studentNumber = int.Parse(studentNumberString);
+
+            Console.Write("Grade: ");
+            string gradeString = Console.ReadLine();
+            int grade = int.Parse(gradeString);
+
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine($"{name}\n{studentNumber}\n{grade}%");
         }
     }
 }
--- 03/StringInterpolation/Program.cs
+++ 03/StringInterpolation/Program.cs
@@ -6,7 +6,17 @@ namespace StringInterpolation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num1 = 48;
+            int num2 = 65;
+
+            Console.WriteLine("The sum of " + num1 + " and " + num2 + " is " + (num1 + num2));
+            Console.WriteLine(string.Format("The sum of {0} and {1} is {2}", num1, num2, num1 + num2));
+            Console.WriteLine($"The sum of {num1} and {num2} is {num1 + num2}");
+
+            Console.WriteLine($"\x48\x65\x6C\x6C\x6F\x20\x57\x6F\x72\x6C\x64\x21");
+            Console.WriteLine(@"\x48\x65\x6C\x6C\x6F\x20\x57\x6F\x72\x6C\x64\x21");
+
+            Console.WriteLine($@"\x{num1}\x{num2}\x6C\x6C\x6F\x20\x57\x6F\x72\x6C\x64\x21");
         }
     }
 }
