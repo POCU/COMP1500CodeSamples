0c1d6b31bdfeb65628833d2b89a6b7494825f3c7
monkeywithbanana <>
2019-04-07 14:46:56 -0700

    remove readline... I'm hardcoding values.. It's easier that way...

--- 04/OrderOfExpressionEvaluation/Program.cs
+++ 04/OrderOfExpressionEvaluation/Program.cs
@@ -6,17 +6,9 @@ namespace OrderOfExpressionEvaluation
     {
         static void Main(string[] args)
         {
-            Console.Write("num1: ");
-            string num1String = Console.ReadLine();
-            int num1 = int.Parse(num1String);
-
-            Console.Write("num2: ");
-            string num2String = Console.ReadLine();
-            int num2 = int.Parse(num2String);
-
-            Console.Write("num3: ");
-            string num3String = Console.ReadLine();
-            int num3 = int.Parse(num3String);
+            int num1 = 5;
+            int num2 = 15;
+            int num3 = 2;
 
             int max1 = num1;
             if (num2 > max1)
