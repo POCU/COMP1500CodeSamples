f10552d96b66253f064cda238b180d40b25deccc
monkeywithbanana <>
2019-04-07 22:13:57 -0700

    Add more string formatting to week3 string interpolation

--- 03/StringInterpolation/Program.cs
+++ 03/StringInterpolation/Program.cs
@@ -15,6 +15,20 @@ namespace StringInterpolation
 
             Console.WriteLine($"\x48\x65\x6C\x6C\x6F\x20\x57\x6F\x72\x6C\x64\x21");
             Console.WriteLine(@"\x48\x65\x6C\x6C\x6F\x20\x57\x6F\x72\x6C\x64\x21");
+
+            Console.WriteLine("{0,-11} + {1,11} = {2}", num1, num2, num1 + num2);
+            Console.WriteLine("{0,-11} - {1,11} = {2}", num1, num2, num1 - num2);
+            Console.WriteLine("{0,-11} * {1,11} = {2}", num1, num2, num1 * num2);
+            Console.WriteLine("{0,-11} / {1,11} = {2:f4}", num1, num2, num1 / (float)num2);
+            Console.WriteLine("{0,-11} % {1,11} = {2}", num2, num1, num2 % num1);
+
+            Console.WriteLine();
+
+            Console.WriteLine(string.Format("{0,-11} + {1,11} = {2}", num1, num2, num1 + num2));
+            Console.WriteLine(string.Format("{0,-11} - {1,11} = {2}", num1, num2, num1 - num2));
+            Console.WriteLine(string.Format("{0,-11} * {1,11} = {2}", num1, num2, num1 * num2));
+            Console.WriteLine(string.Format("{0,-11} / {1,11} = {2:f4}", num1, num2, num1 / (float)num2));
+            Console.WriteLine(string.Format("{0,-11} % {1,11} = {2}", num2, num1, num2 % num1));
         }
     }
 }
