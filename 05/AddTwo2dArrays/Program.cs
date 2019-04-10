78669f80726853fa8bae6837579d6fe09905a16b
monkeywithbanana <>
2019-04-09 23:32:51 -0700

    UPPER_CASE for CONSTS!!!!

--- 05/AddTwo2dArrays/Program.cs
+++ 05/AddTwo2dArrays/Program.cs
@@ -6,10 +6,10 @@ namespace AddTwo2dArrays
     {
         static void Main(string[] args)
         {
-            const int rowCount = 4;
-            const int columnCount = 5;
+            const int ROW_COUNT = 4;
+            const int COLUMN_COUNT = 5;
 
-            int[,] array1 = new int[rowCount, columnCount]
+            int[,] array1 = new int[ROW_COUNT, COLUMN_COUNT]
             {
                 { 2, 3, 4, 10, 5 },
                 { 1, 11, -4, 4, 6 },
@@ -17,7 +17,7 @@ namespace AddTwo2dArrays
                 { 7, 17, 2, -4, 2 }
             };
 
-            int[,] array2 = new int[rowCount, columnCount]
+            int[,] array2 = new int[ROW_COUNT, COLUMN_COUNT]
             {
                 { -2, 7, 10, -10, 1 },
                 { 2, 7, -6, -13, 6 },
@@ -25,11 +25,11 @@ namespace AddTwo2dArrays
                 { 7, 8, 7, -9, 1 }
             };
 
-            int[,] sumArray = new int[rowCount, columnCount];
+            int[,] sumArray = new int[ROW_COUNT, COLUMN_COUNT];
 
-            for (int i = 0; i < rowCount; i++)
+            for (int i = 0; i < ROW_COUNT; i++)
             {
-                for (int j = 0; j < columnCount; j++)
+                for (int j = 0; j < COLUMN_COUNT; j++)
                 {
                     sumArray[i, j] = array1[i, j] + array2[i, j];
                 }
@@ -37,9 +37,9 @@ namespace AddTwo2dArrays
 
             Console.WriteLine("---Let's print this!---");
 
-            for (int i = 0; i < rowCount; i++)
+            for (int i = 0; i < ROW_COUNT; i++)
             {
-                for (int j = 0; j < columnCount; j++)
+                for (int j = 0; j < COLUMN_COUNT; j++)
                 {
                     Console.Write("{0,10} ", sumArray[i, j]);
                 }
