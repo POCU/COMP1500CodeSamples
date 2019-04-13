e7a2861d4f7b11ecf0b444841c9dfb729a88b72e
monkeywithbanana <>
2019-04-13 14:44:03 -0700

    Adding scope example

---
+++ 06/Scope/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Scope
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int THRESHOLD = 10;
+
+            int[] numbers = new int[] { 1, -3, 4, -10, 4, 2, 9, 15 };
+            int average = GetAverage(numbers);
+
+            Console.WriteLine($"numbers: {string.Join(", ", numbers)}");
+
+            if (average > THRESHOLD)
+            {
+                //int average = 0; // Compile error!
+                string message = "Message in the if statement!";
+                Console.WriteLine(message);
+            }
+            else
+            {
+                //int average = 0; // Compile error!
+                string message = "Message in the else statement!";
+                Console.WriteLine(message);
+            }
+        }
+
+        static int GetAverage(int[] numbers)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = 0;
+            }
+
+            return sum;
+        }
+    }
+}
