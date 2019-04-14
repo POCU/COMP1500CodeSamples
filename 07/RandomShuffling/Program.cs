6be4a70921f7335f7ea1f887491cd8fab3d54954
monkeywithbanana <>
2019-04-14 14:59:16 -0700

    Add week 7 code samples

---
+++ 07/RandomShuffling/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace RandomShuffling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int SEED = 0;
+            int[] numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+
+            Console.WriteLine("Before shuffling:");
+            Console.WriteLine($"[{string.Join(", ", numbers)}]");
+
+            Random random = new Random(SEED);
+
+            for (int i = numbers.Length - 1; i > 0; i--)
+            {
+                int j = random.Next(0, i);
+                int temp = numbers[j];
+                numbers[j] = numbers[i];
+                numbers[i] = temp;
+            }
+
+            Console.WriteLine("Before shuffling:");
+            Console.WriteLine($"[{string.Join(", ", numbers)}]");
+        }
+    }
+}
---
+++ 07/RecursiveFactorial/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace RecursiveFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const ulong FACTORIAL = 10;
+
+            Console.WriteLine("NonRecursiveFactorial:");
+            Console.WriteLine(NonRecursiveFactorial(FACTORIAL));
+
+            Console.WriteLine("RecursiveFactorial:");
+            Console.WriteLine(RecursiveFactorial(FACTORIAL));
+        }
+
+        static ulong NonRecursiveFactorial(ulong n)
+        {
+            if (n <= 1)
+            {
+                return 1;
+            }
+
+            uint factorial = 1;
+
+            for (uint i = 2; i <= n; i++)
+            {
+                factorial *= i;
+            }
+
+            return factorial;
+        }
+
+        static ulong RecursiveFactorial(ulong n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+
+            return RecursiveFactorial(n - 1) * n;
+        }
+    }
+}
