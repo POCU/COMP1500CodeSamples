258b485638abf67e526ba5a1285f31a14f7c9abd
monkeywithbanana <>
2019-04-15 20:23:12 -0700

    function overloading and default parameters example

--- 09/FunctionOverloadingVsDefaultParameters/Program.cs
+++ 09/FunctionOverloadingVsDefaultParameters/Program.cs
@@ -6,7 +6,51 @@ namespace FunctionOverloadingVsDefaultParameters
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Foo();
+            Foo(10);
+            Foo("POCU");
+            Foo("POCU", "COMP1500", "Intro to programming!");
+
+            Console.WriteLine("--------------------------------------");
+
+            Bar("POCU");
+            Bar("POCU", "COMP1500");
+            Bar("POCU", "COMP1500", "Intro to programming!");
+        }
+
+        static void Foo()
+        {
+            Console.WriteLine("Foo with no arguments.");
+        }
+
+        static void Foo(int x)
+        {
+            Console.WriteLine($"Foo with {typeof(int).Name}: {x} as an argument.");
+        }
+
+        //static int Foo(int x)
+        //{
+        //    return x + 1;
+        //}
+
+        static void Foo(string s)
+        {
+            Console.WriteLine($"Foo with {typeof(string).Name}: {s} as an argument.");
+        }
+
+        static void Foo(string s1, string s2, string s3)
+        {
+            Console.WriteLine($"Foo with {typeof(string).Name}: {s1}, {typeof(string).Name}: {s2} and {typeof(string).Name}: {s3} as an argument.");
+        }
+
+        //static void Foo(string s, string s2 = "", string s3 = "")
+        //{
+        //    Console.WriteLine($"Foo with {typeof(string).Name}: {s}, {typeof(string).Name}: {s2} and {typeof(string).Name}: {s3} as an argument.");
+        //}
+
+        static void Bar(string s, string s2 = "", string s3 = "")
+        {
+            Console.WriteLine($"Bar with {typeof(string).Name}: {s}, {typeof(string).Name}: {s2} and {typeof(string).Name}: {s3} as an argument.");
         }
     }
 }
