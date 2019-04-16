c960cbbe54275431aeb51342763694e8442b92d2
monkeywithbanana <>
2019-04-15 23:22:18 -0700

    Adding out parameter example

--- 09/OutParameterModifier/Program.cs
+++ 09/OutParameterModifier/Program.cs
@@ -6,7 +6,49 @@ namespace OutParameterModifier
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter \"true\" or \"false\":");
+            string booleanString = Console.ReadLine();
+
+            if (bool.TryParse(booleanString, out bool b))
+            {
+                Console.WriteLine($"Successfully parsed: {b}");
+            }
+            else
+            {
+                Console.WriteLine("Cannot be parsed to boolean");
+            }
+
+            Console.WriteLine("Enter an integer:");
+            string intString = Console.ReadLine();
+
+            if (int.TryParse(intString, out int number))
+            {
+                Console.WriteLine($"Successfully parsed: {number}");
+            }
+            else
+            {
+                Console.WriteLine("Cannot be parsed to integer");
+            }
+
+            int someNumber = 5;
+
+            if (TryGetIntegerGreaterThan(someNumber, out int output))
+            {
+                Console.WriteLine($"Great! {output} > {someNumber}");
+            }
+            else
+            {
+                Console.WriteLine($"Failed to get an integer greater than {someNumber}");
+            }
+        }
+
+        static bool TryGetIntegerGreaterThan(int input, out int output)
+        {
+            var random = new Random();
+
+            output = random.Next(0, 10);
+
+            return output > input;
         }
     }
 }
