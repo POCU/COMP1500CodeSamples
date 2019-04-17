738b58e67e78a051d46e389648bc727874fd13a1
monkeywithbanana <>
2019-04-16 22:25:29 -0700

    Adding week 10 skeleton

---
+++ 10/Dictionary/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 10/StringBuilder/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StringBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // StringBuilder
+            // Append
+            // AppendLine
+            // Insert
+            // Replace
+            // Remove
+            // Clear
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
