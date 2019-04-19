f43704906219637f66bfa09d29dfb39719f0c278
monkeywithbanana <>
2019-04-19 16:38:42 -0700

    Add more code samples for week 10

---
+++ 10/FloatVsDoubleVsDecimal/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace FloatVsDoubleVsDecimal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float[] floats = new float[] { 2.342f, 9.326f, 3.445f, 5.713f, 2.458f, 3.689f, 10.121f, 4.786f, 6.664f };
+
+            float averageFloat = GetAverage(floats);
+
+            Console.WriteLine($"Average in float: {averageFloat}");
+
+            double[] doubles = new double[] { 2.342, 9.326, 3.445, 5.713, 2.458, 3.689, 10.121, 4.786, 6.664 };
+
+            double averageDouble = GetAverage(doubles);
+
+            Console.WriteLine($"Average in double: {averageDouble}");
+
+            decimal[] decimals = new decimal[] { 2.342m, 9.326m, 3.445m, 5.713m, 2.458m, 3.689m, 10.121m, 4.786m, 6.664m };
+
+            decimal averageDecimal = GetAverage(decimals);
+
+            Console.WriteLine($"Average in decimal: {averageDecimal}");
+        }
+
+        static float GetAverage(float[] floats)
+        {
+            float sum = 0;
+            foreach (float f in floats)
+            {
+                sum += f;
+            }
+
+            return sum / floats.Length;
+        }
+
+        static double GetAverage(double[] doubles)
+        {
+            double sum = 0;
+            foreach (double d in doubles)
+            {
+                sum += d;
+            }
+
+            return sum / doubles.Length;
+        }
+
+        static decimal GetAverage(decimal[] decimals)
+        {
+            decimal sum = 0;
+            foreach (decimal d in decimals)
+            {
+                sum += d;
+            }
+
+            return sum / decimals.Length;
+        }
+    }
+}
---
+++ 10/StringConcatVsStringBuilder/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+
+namespace StringConcatVsStringBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int LOOP_COUNT = 1000;
+
+            Stopwatch stopWatch = new Stopwatch();
+
+            #region USING_CONCATENATION
+            stopWatch.Start();
+
+            string concatenated = string.Empty;
+            for (int i = 0; i < LOOP_COUNT; i++)
+            {
+                concatenated += "test";
+            }
+
+            stopWatch.Stop();
+            Console.WriteLine($"Time elapsed in ms (Concatenated): {stopWatch.Elapsed.TotalMilliseconds}");
+            #endregion
+
+            stopWatch.Reset();
+
+            #region USING_STRING_BUILDER
+            stopWatch.Start();
+
+            StringBuilder stringBuilder = new StringBuilder(4096);
+            for (int i = 0; i < LOOP_COUNT; i++)
+            {
+                stringBuilder.Append("test");
+            }
+
+            string s = stringBuilder.ToString();
+
+            stopWatch.Stop();
+            Console.WriteLine($"Time elapsed in ms (StringBuilder): {stopWatch.Elapsed.TotalMilliseconds}");
+            #endregion
+        }
+    }
+}
