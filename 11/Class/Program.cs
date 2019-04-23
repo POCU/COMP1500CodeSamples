a79c7e716e703f6e82a39f2467182ae79e3db5ec
monkeywithbanana <>
2019-04-22 17:45:13 -0700

    adding sample codes for week 11 (WIP)

---
+++ 11/Class/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 11/ExtensionMethod/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExtensionMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
