9e72778371a6cc73a4de9e44a6d570dd2d3b1d01
monkeywithbanana <>
2019-04-29 13:04:52 -0700

    add code sample projects for week 13

---
+++ 13/ReadAndWriteFile/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReadAndWriteFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 13/ReadAndWriteFileUsingFileStream/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReadAndWriteFileUsingFileStream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
