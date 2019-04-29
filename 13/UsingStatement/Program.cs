486055ddee2a1d52114c6ea1f5c80db9aed3cb64
monkeywithbanana <>
2019-04-29 15:56:28 -0700

    UsingStatement example done

--- 13/UsingStatement/Program.cs
+++ 13/UsingStatement/Program.cs
@@ -1,12 +1,70 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace UsingStatement
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static readonly string CURRENT_DIRECTORY = Directory.GetCurrentDirectory();
+        private static readonly string OUTPUT_FOLDER_PATH = Path.Combine(CURRENT_DIRECTORY, "output");
+        private static readonly string INPUT_FILE_FULL_PATH = Path.Combine(CURRENT_DIRECTORY, "input", "inputtext.txt");
+        private static readonly string OUTPUT_FILE_FULL_PATH = Path.Combine(CURRENT_DIRECTORY, "output", "outputtext.txt");
+
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            setup();
+
+            string allText = string.Empty;
+            List<string> allLines = new List<string>();
+            using (StreamReader reader = new StreamReader(File.Open(INPUT_FILE_FULL_PATH, FileMode.Open, FileAccess.Read)))
+            {
+                Console.WriteLine("----------------------------------");
+                allText = reader.ReadToEnd();
+                Console.WriteLine(allText);
+                Console.WriteLine("----------------------------------");
+
+                reader.BaseStream.Seek(0, SeekOrigin.Begin);
+
+                allLines = new List<string>();
+                while (!reader.EndOfStream)
+                {
+                    allLines.Add(reader.ReadLine());
+                }
+
+                foreach (string line in allLines)
+                {
+                    Console.WriteLine(line);
+                }
+
+                Console.WriteLine("----------------------------------");
+            }
+
+            using (StreamWriter writer = new StreamWriter(File.Open(OUTPUT_FILE_FULL_PATH, FileMode.OpenOrCreate, FileAccess.Write)))
+            {
+                writer.Write(allText);
+
+                foreach (string line in allLines)
+                {
+                    writer.WriteLine(line);
+                }
+
+                writer.BaseStream.Seek(0, SeekOrigin.Begin);
+                writer.Write("Overwritten text");
+            }
+        }
+
+        private static void setup()
+        {
+            if (File.Exists(OUTPUT_FILE_FULL_PATH))
+            {
+                File.Delete(OUTPUT_FILE_FULL_PATH);
+            }
+
+            if (!Directory.Exists(OUTPUT_FOLDER_PATH))
+            {
+                Directory.CreateDirectory(OUTPUT_FOLDER_PATH);
+            }
         }
     }
 }
